f4b0b6109252b129349eb9bde11ecf16cf156da9
Slava <slava@example.com>
2020-01-20 13:57:57 +0300

    dell HelloWorld.vue

---
+++ src/components/Contacts.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+        <v-layout>
+            <v-card>
+                <v-card-title>Contacts</v-card-title>
+                <v-card-text>OOO My Project</v-card-text>
+                <p class="ma-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid enim in, iste nihil, numquam omnis placeat quae quis reiciendis, rem ullam ut. Adipisci alias asperiores id laborum nobis pariatur.</p>
+            </v-card>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Contacts"
+    }
+</script>
+
+<style scoped>
+
+</style>
